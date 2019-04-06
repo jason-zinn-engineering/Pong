3506b458c7247663b41123b9f9ce65ec999237fb
Jason Zinn <jason.zinn@ucalgary.ca>
2019-04-06 02:23:44 -0600

    Step 1

---
+++ Game.py
@@ -0,0 +1,67 @@
+#-------------------------------------------------------------------------------
+#@Author Jason A. Zinn
+#@Date April 6th, 2019
+# Python Implementation of Classic Arcade Game Pong
+#-------------------------------------------------------------------------------
+import pygame
+from pygame.locals import *
+import math
+#-------------------------------------------------------------------------------
+# Global Variables
+#-------------------------------------------------------------------------------
+player_one_left = False
+player_two_right = False
+player_computer = True
+#-------------------------------------------------------------------------------
+# Functions
+#-------------------------------------------------------------------------------
+def Game():
+    MAX_HEIGHT = 512
+    MAX_WIDTH = 512
+    def __init__(self):
+        pass
+    def start(self):
+        pass
+#
+#
+# Superclass Player
+class player:
+
+    def __init__(self, starting_side, keyboard_inputs):
+        pass
+    def player_move():
+        pass
+# Subclass Player
+class player_left(player):
+    def __init__(self):
+        pass
+# Subclass PLayer
+class player_right(player):
+    def __init__(self):
+        pass
+#
+#
+# Ball Superclass
+class ball:
+
+    def __init__(self):
+        pass
+# Vector Class
+class vector:
+    def __init__(self):
+        pass
+
+
+
+
+
+#-------------------------------------------------------------------------------
+# Main Function
+#-------------------------------------------------------------------------------
+def main():
+    pass
+#-------------------------------------------------------------------------------
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode((Game.MAX_WIDTH, Game.MAX_HEIGHT))
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Pong
-Pong Implemented in Python 3.7.3
+Classic Arcade Game Pong Implemented in Python 3.7.3
+# Jason A. Zinn
+@Start April 6th, 2019
