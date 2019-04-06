3d5070b5ba94bb085901d38e63d6e6fd9898bbb6
Jason Zinn <jason.zinn@ucalgary.ca>
2019-04-06 03:34:02 -0600

    Brisk Iced Tea

--- ball.py
+++ ball.py
@@ -1,6 +1,16 @@
 import pygame
 import math
+import Game
 class ball:
-
-    def __init__(self):
-        pass
+#
+    def __init__(self, surface, color, pos, radius, width=0):
+        self.surface = Game.screen
+        self.color = (255,255,0)
+        self.pos = (0,0)
+        self.radius = 10
+    #
+    def draw_ball_frame(self):
+        return pygame.draw.circle(surface, color, pos, radius, width)
+    #
+    def draw_ball_loop(self, draw_ball_frame):
+        self.draw_ball_frame = draw_ball_frame()
--- board.py
+++ board.py
@@ -1,18 +1,19 @@
 import vector
 import Game
 import pygame
+import math
 class board:
-    # variables
-    HEIGHT = 10
-    WIDTH = 2
-    
     # constructor
-    def __init__(self, vector):
-        screen = pygame.display.get_surfacE()
-        self.area = screen.get_rect()
-
-        
+    def __init__(self, color, vector, height, width):
+        self.color = (255,255,0)
+        self.vector = vector
+        self.height = 10
+        self.width = 2 
     # functions
-    def draw_board(self, color):
+    def draw_board_frame(self):
         pygame.draw.rect(Game.screen, color, vector)
+    #
+    def draw_board_loop(self, draw_frame):
+        self.draw_frame = draw_frame
+
         
