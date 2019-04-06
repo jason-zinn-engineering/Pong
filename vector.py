dad959ddb8906671bfed945a29e9865769697f78
AlejandroGarcia2 <alejandro.garcia@ucalgary.ca>
2019-04-06 03:13:27 -0600

    More work on vectr

--- vector.py
+++ vector.py
@@ -1,3 +1,5 @@
+import math
+
 class vector:
     def __init__(self):
         pass
@@ -6,3 +8,17 @@ class vector:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def length()
+        return math.sqrt(x*x + y*y)
+
+    def __add__(self, other):
+        self.x += other.x
+        self.y += other.y
+
+    def __sub__(self, other):
+        self.x -= other.x
+        self.y -= other.y
+
+    def __mul__(self, other);
+        return self.x*other.x + self.y*other.y
